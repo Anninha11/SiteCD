47fecfe010fd0af4f7186aa84bacae61b71b0ef7
annalegey <133767348+annalegey@users.noreply.github.com>
2023-06-15 22:28:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def main():
         imagem = "logodeals.png"
 #define a barra lateral 
     with st.sidebar:
-        st.title('Beauty Deals')  st.image(imagem,use_column_width=False)
+        st.title('Beauty Deals') + st.image(imagem,use_column_width=False)
 #Exibe a imagem no Streamlit
         with st.form(key='form_busca'):
 #cria uma caixa de texto para o usuário digitar o produto que deseja buscar 
